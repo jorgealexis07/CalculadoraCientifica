959024d42cfe9f9f55dc5d48c3d93ca389e9004b
jorgealexis07 <78820449+jorgealexis07@users.noreply.github.com>
2021-10-06 15:30:11 -0500

    Add files via upload

---
+++ Calculadora cientifica/Calculadora cientifica/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_cientifica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }   
+
+        //Botones numericos
+        private void Button_N1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Bottun_N2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Bottun_N3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Bottun_N4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Bottun_N5_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Bottun_N6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Bottun_N7_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Bottun_N8_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Bottun_N9_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button_N0_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón punto decimal 
+        private void Button_Decimal_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón de la función de  suma
+        private void Button_Suma_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón de la función de  resta
+        private void Button_Resta_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón de la función de  multiplicar
+        private void Button_Multiplicacion_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón  de la función de dividir
+        private void Button_Division_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Método con las operaciones de + - * / 
+        private void Operaciones(double valor1, double valor2)
+        {
+            
+        }
+        //Botón  de la función de igual
+        private void Button_gual_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón con la función de RAIZ CUADRADA
+        private void Button_RaizCuadrada_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón con la función de PORCENTAJE
+        private void Button_Porcentaje_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón que realiza la operacion inversa 1/x
+        private void Button_1x_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón con la función de C
+        private void Button_C_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón con la función de CE
+        private void Button_CE_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón con la función de RETROCEDER
+        private void Button_Retroceso_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón con la función de cambio de SIGNO
+        private void Button_Signo_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        //Botón seno
+        private void Button_sin_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón coseno
+        private void Button_Cos_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón tangente
+        private void Button_Tan_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón seno inverso
+        private void Button_Asin_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón coseno inverso
+        private void Button_Acos_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón tangente inverso
+        private void Button_Atan_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón que devuelve el valor de Pi
+        private void BtnPi_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón que eleva a x a la -1
+        private void Button_Xmenos1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón que eleva un numero a x Potencia
+        private void Button_Exponente_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón que devuelve el valor de e
+        private void Btne_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón que saca la raiz x de un numero
+        private void Button_RaizX_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón que eleva a x al cubo
+        private void Button_Cubo_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón que devuelve el factorial de un numero
+        private void Button_Factorial_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón de la raíz cubica
+        private void Button_RaizCubica_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón eleva a x al cuadrado
+        private void Button_Cuadrado_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón que eleva 10 a la x potencia
+        private void Button_10levarX_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón de la exponenciación neperiana
+        private void Button_EXP_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón del logaritmo neperiano
+        private void Button_log_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón con la función MC
+        private void Button_MC_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón con la función MR
+        private void Button_MR_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //Botón con la función MS
+        private void BtnMS_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón con la función M+
+        private void Button_Mmas_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Botón con la función M-
+        private void Button_Mmenos_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
